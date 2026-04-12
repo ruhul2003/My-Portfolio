349d1bab081a791898b6f28d15cd4701fa15cccb
ruhul2003 <ruhul941020@gmail.com>
2026-04-13 02:21:42 +0600

    Added navlinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-
-import './App.css'
+import Navbar from "./Layout/Navbar"
+import About from "./Sections/About"
+import Projects from "./Sections/Projects"
+import Testimonials from "./Sections/Testimonials"
+import Contact from "./Sections/Contact"
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+
+      <main>
+        {/* <About />
+        <Projects/>
+        <Testimonials/>
+        <Contact/> */}
+      </main>
+    </div>
   )
 }
 
---
+++ src/Layout/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const NavLinks = [
+    { href: '#about', name: 'About' },
+    { href: '#projects', name: 'Projects' },
+    { href: '#experience', name: 'Experience' },
+    { href: '#testimonials', name: 'Testimonials' },
+    { href: '#contact', name: 'Contact' }
+]
+
+const Navbar = () => {
+    return (
+        <header className='w-9/12 mx-auto py-5 bg-transparent'>
+            <nav className='w-full flex justify-between'>
+                <a href='#' className='text-2xl font-bold'>
+                    Ruhul <span>Amin</span>
+                </a>
+                <ul className='flex justify-center items-center gap-5 text-lg font-medium'>
+                    {NavLinks.map((link, index) => (
+                        <a key={index} href={link.href} className='hover:text-blue-500'>
+                            {link.name}
+                        </a>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
