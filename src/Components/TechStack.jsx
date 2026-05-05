acd9673cfd2a94a8f658ec55457506dac437082b
ruhul2003 <ruhul941020@gmail.com>
2026-05-05 17:20:57 +0600

    Added some section

--- src/Components/TechStack.jsx
+++ src/Components/TechStack.jsx
@@ -4,6 +4,7 @@ const TechStack = () => {
     return (
         <div>
             <h1 className="text-3xl md:text-4xl text-center font-extrabold text-[#C4F000]">TechStack</h1>
+            <h2 className="text-xl md:text-2xl text-center font-bold text-white">My Technical Skills</h2>
         </div>
     );
 };
